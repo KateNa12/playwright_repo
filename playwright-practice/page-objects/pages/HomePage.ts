e4c662dc633510d42e60aa0fab60d8545f6763ba
Kateryna Naimark <kateryna.naimark@gmail.com>
2025-02-01 16:32:33 +0200

    hw27 was added

--- playwright-practice/page-objects/pages/HomePage.ts
+++ playwright-practice/page-objects/pages/HomePage.ts
@@ -3,15 +3,22 @@ import { Locator, Page } from '@playwright/test';
 export default class HomePage {
     readonly page: Page;
     readonly signUpButton: Locator;
+    readonly signInButton : Locator;
 
     constructor(page: Page) {
         this.page = page;
         this.signUpButton = page.locator('.btn-primary');
+        this.signInButton = page.locator('button.header_signin');
     }
 
     async clickSingUpPutton() {
         await this.signUpButton.click();
     }
+
+    async clickSignInButton(){
+        await this.signInButton.click();
+    }
+
 
     async openPage() {
         await this.page.goto('/');
--- playwright-practice/playwright.config.ts
+++ playwright-practice/playwright.config.ts
@@ -36,10 +36,15 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
+      name: 'setup',
+      use: { ...devices['Desktop Chrome'] },
+      testMatch: '*setup/*.ts'
+    },
+    {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-    },
-
+      // dependencies: ['setup']
+    }
     // {
     //   name: 'firefox',
     //   use: { ...devices['Desktop Firefox'] },
