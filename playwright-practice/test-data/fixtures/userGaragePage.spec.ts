87eedcc076269447316eb54722d0aab5278d8e6f
Kateryna Naimark <kateryna.naimark@gmail.com>
2025-02-09 21:04:48 +0200

    hw28 fixed

--- playwright-practice/test-data/fixtures/userGaragePage.spec.ts
+++ playwright-practice/test-data/fixtures/userGaragePage.spec.ts
@@ -1,3 +1,4 @@
+
 import { test as base } from '@playwright/test';
 import GaragePage from '../../page-objects/pages/GaragePage';
 import HomePage from '../../page-objects/pages/HomePage';
@@ -7,20 +8,22 @@ let homePage: HomePage;
 let signInForm: SignInForm;
 let garagePage: GaragePage;
 
-export const test = base.extend({
-    garagePageAsLoggedMainUser: async ({ page }, use) => {
-        test.use({storageState: './test-data/states/mainUserState.json'});
+
+export const test = base.extend<{garagePageAsLoggedMainUser: GaragePage }>({
+              
+    garagePageAsLoggedMainUser: async ({ browser }, use) => {
+        const context = await browser.newContext({ storageState: './test-data/states/mainUserState.json' });
+        const page = await context.newPage();
+
+       
         homePage = new HomePage(page);
         signInForm = new SignInForm(page);
         garagePage = new GaragePage(page);
 
-        // await homePage.openPage();
-        // await homePage.clickSignInButton();
-        // await signInForm.loginWithCredentials('kateryna.naimark+2@gmail.com', 'pvGvlLZ1QtQuXdm');
-        // await garagePage.verifyPageIsOpen();
-
-        // await page.context().storageState({ path: './test-data/states/mainUserState.json' });
-        
+        await homePage.openPage();
+        await homePage.clickSignInButton();
+        await signInForm.loginWithCredentials('kateryna.naimark+2@gmail.com', 'pvGvlLZ1QtQuXdm');
+        await garagePage.verifyPageIsOpen();          
         
         await use(garagePage);
 
--- playwright-practice/tests/setup/LoginAndSaveState.spec.ts
+++ playwright-practice/tests/setup/LoginAndSaveState.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect, Locator } from '@playwright/test';
+import { test, chromium, expect, Locator } from '@playwright/test';
 import HomePage from '../../page-objects/pages/HomePage';
 import SignInForm from '../../page-objects/pages/SignInForm';
 import GaragePage from '../../page-objects/pages/GaragePage';
@@ -8,19 +8,30 @@ test.describe(("Setup user"), () => {
     let signInForm: SignInForm;
     let garagePage: GaragePage;
 
-    test.beforeEach(async ({ page }) => {
+    test.beforeAll(async ({ page }) => {
         homePage = new HomePage(page);
         signInForm = new SignInForm(page);
         garagePage = new GaragePage(page);
     });
 
-    test('Log in and save main user state', async ({ page }) => {
+    test('Log in and save main user state', async () => {
+        const browser = await chromium.launch();
+        const context = await browser.newContext();
+
+        const page = await context.newPage();
+        // await page.goto('https://guest:welcome2qauto@qauto.forstudy.space');
+        // await page.click('button.header_signin');
+        // await page.fill('//input[@id="signinEmail"]', 'kateryna.naimark+2@gmail.com');
+        // await page.fill('//input[@id="signinPassword"]', 'pvGvlLZ1QtQuXdm');
+        // await page.click('//div[@class="modal-content"]//button[@class="btn btn-primary"]');
+
+
         await homePage.openPage();
         await homePage.clickSignInButton();
         await signInForm.loginWithCredentials('kateryna.naimark+2@gmail.com', 'pvGvlLZ1QtQuXdm');
         await garagePage.verifyPageIsOpen();
 
-        await page.context().storageState({ path: './test-data/states/mainUserState.json' });
+        await context.storageState({ path: './test-data/states/mainUserState.json' });
     });
 
 });
